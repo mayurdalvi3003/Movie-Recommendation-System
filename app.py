24cee40652943ccfd5782c0293c8e841d6f6917d
mayurdalvi3003 <mayurdalvi3003@gmail.com>
2024-12-21 00:21:03 +0530

    modified app.py

--- app.py
+++ app.py
@@ -51,10 +51,10 @@ def recommend(movie):
 
 
     
-movies_dict =pickle.load(open('movie_dict.pkl ','rb'))
+movies_dict =pickle.load(open('movie_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('similarity.pkl ','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
 
 
 #st.write("Movie Recommender System")
